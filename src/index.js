8f925ab71e6f9652efb5afefa7154f1a34ab644d
nrfmkrhr <77868239+nrfmkrhr@users.noreply.github.com>
2021-10-01 10:58:49 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// // const val4 = {
+// //   name: "norifumi",
+// //   age: 58,
+// // };
+// // console.log(val4);
+
+// // val4.addres = "setagaya",
+// // console.log(val4);
+
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+// const name = "kirihara";
+// const age = 58;
+// console.log(name,age);
+
+// // const message1 = "私の名前は" + name + "です。年齢は" + age +"才です。";
+// // console.log(message1);
+// const message2 = `私の名前は${name}です。年齢は${age}才です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+function func1(str) {
+  return str;
+}
+console.log(func1("func1です。"));
